ab0d190c35e4ae658ffc5356bf668b2b7c9bd445
arun170790 <arunkumar.thangaraj17@gmail.com>
2018-11-21 18:32:31 +0530

    First project commit

---
+++ src/test/java/github_ng/Masterscript.java
@@ -0,0 +1,21 @@
+package github_ng;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class Masterscript {
+  @Test
+  public void Maximo() {
+	  
+	  System.out.println("Hello universe!");
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+}
