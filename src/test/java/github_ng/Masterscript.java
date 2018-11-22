361d189173f7221d9b0a4e0be3d1f93684883288
arun170790 <arunkumar.thangaraj17@gmail.com>
2018-11-22 14:18:02 +0530

    Parallel execution
    Added parallel execution
    1. launch chrome
    2. launch IE
    with extent reports support
    - The result file will be overridden

--- src/test/java/github_ng/Masterscript.java
+++ src/test/java/github_ng/Masterscript.java
@@ -1,21 +1,76 @@
 package github_ng;
 
 import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 
 public class Masterscript {
+	
+	public static WebDriver driver;
+	public static ExtentReports reporter;
+	public static ExtentTest test_report;
+	
+	@BeforeClass
+	public void buildup() {
+		
+		reporter = new ExtentReports(System.getProperty("user.dir") + "\\test-output\\Extent_reports\\TestResults.html",true); 
+		
+	}
+	
+	 @BeforeMethod
+	  public void beforeMethod() {
+		 
+		 test_report = reporter.startTest("E2E Testcase");
+	  }
+	
+	
   @Test
   public void Maximo() {
 	  
-	  System.out.println("Hello universe!!!");
+	  ChromeOptions options = new ChromeOptions();
+	  options.setExperimentalOption("useAutomationExtension", false);
+	  options.addArguments("start-maximized");
+	  options.addArguments("chrome.switches","--disable-extensions");
+	  options.addArguments("--disable-dev-shm-usage");
+	
+	  driver = new ChromeDriver(options);
+	  test_report.log(LogStatus.PASS, "Chrome browser launched");
+	  
   }
-  @BeforeMethod
-  public void beforeMethod() {
+  
+  @Test
+  public void Salesforce() {
+	  
+	  String IEfilepath = "C:\\Users\\athangaraj001\\Documents\\ng_poc\\src\\test\\resources\\IEDriverServer.exe";
+	  System.setProperty("webdriver.ie.driver", IEfilepath);
+	  driver = new InternetExplorerDriver();
+	  driver.manage().window().maximize();
+	  test_report.log(LogStatus.PASS, "Internet Explorer launched");
   }
+  
+  
+ 
 
   @AfterMethod
   public void afterMethod() {
+  }
+  
+  @AfterClass
+  public void teardown() {
+	  
+	  reporter.endTest(test_report);
+	  reporter.flush();
   }
 
 }
